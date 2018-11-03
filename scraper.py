16b1b5358b02a2d8f62005df315f3e0de33a9368
Khalid Hourani <khalid.hourani@gmail.com>
2018-11-03 16:50:27 -0500

    Implemented webpage parse with urllib.

--- scraper.py
+++ scraper.py
@@ -2,6 +2,10 @@
 
 from bs4 import BeautifulSoup as bs
 import requests
+import urllib.request
+
+def price(string):
+    return string[string.find('$') + 1:]
 
 def create_url(start, end, date, children=0, adults=1, seniors=0):
     start = '%20'.join(start.split(' '))
@@ -10,16 +14,18 @@ def create_url(start, end, date, children=0, adults=1, seniors=0):
     return url
 
 def parse_page(start, end, date, children=0, adults=1, seniors=0):
-        page = requests.get(create_url(start, end, date, children, adults, seniors))
-        soup = bs(page.content, 'html.parser')
-        departure_times = soup.find_all('span', {'data-test-id': 'departure-time'})
-        arrival_times = soup.find_all('span', {'data-test-id': 'arrival-time'})
-        duration = soup.find_all('span', {'data-test-id': 'duration'})
-        num_stops = soup.find_all('span', {'data-test-num-stops': '0'})
-        seats_left = soup.find_all('span', {'class': 'primary-sub-content', 'data-test-id': 'seats-left'})
-        price = soup.find_all('span', {'data-test-id': 'listing-price-dollars'})
-        print(departure_times, arrival_times, duration, num_stops, seats_left, price)
-        return zip(departure_times, arrival_times, duration, num_stops, seats_left, price)
+    url = create_url(start, end, date, children, adults, seniors)
+    page = urllib.request.urlopen(url)
+    soup = bs(page.read(), 'html.parser')
+    departure_times = [html.text.replace('\n', '').replace(' ','') for html in soup.find_all('span', {'data-test-id': 'departure-time'})]
+    arrival_times = [html.text for html in soup.find_all('span', {'data-test-id': 'arrival-time'})]
+    durations = [html.text.replace('\n', '').replace(' ','')  for html in soup.find_all('span', {'data-test-id': 'duration'})]
+    num_stops = [html.text.replace('\n', '').replace(' ','')  for html in soup.find_all('span', {'data-test-num-stops': '0'})]
+    #seats_left = soup.find_all('span', {'class': 'primary-sub-content', 'data-test-id': 'seats-left'})
+    prices = [price(html.text) for html in soup.find_all('h3', {'class': 'visuallyhidden', 'data-test-id': 'result-header'})]
+    #print(departure_times, arrival_times, durations, num_stops, prices)
+    return zip(departure_times, arrival_times, durations, num_stops, prices)
             
 
-parse_page('new york city, ny', 'miami, florida', '12/01/2018')
+for x in parse_page('new york city, ny', 'miami, florida', '12/01/2018'):
+    print(x)
