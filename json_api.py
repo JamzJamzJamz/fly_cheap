a9a73ea71edef69e9ce522871e0ec3f4cb82a31c
Khalid Hourani <khalid.hourani@gmail.com>
2018-11-04 04:29:46 -0600

    Updated Formatting

--- json_api.py
+++ json_api.py
@@ -1,6 +1,39 @@
 import requests
 import json
 
+def time_format(time):
+    hours, minutes = [int(x) for x in time.split(':')]
+    days = hours // 24
+    if days:
+        if days == 1:
+            day_string = '1 day'
+        else:
+            day_string = '{0} days'.format(days)
+    else:
+        day_string = ''
+    if hours:
+        if hours == 1:
+            hour_string = '1 hour'
+        else:
+            hour_string = ' {0} hours'.format(hours)
+    else:
+        hour_string = ''
+    if minutes:
+        if minutes == 1:
+            minute_string = '1 minute'
+        else:
+            minute_string = ' {0} minutes'.format(minutes)
+    else:
+        minute_string = ''
+    duration = day_string + hour_string + minute_string
+    if duration[0] == ' ':
+        duration = duration[1:]
+    if days == 0 and hours == 0:
+        return minute_string[1:]
+    else:
+        return duration
+    
+
 def pull_data(origin, departure, date, api_key='oIkAPQAiwHwJc7cUHCblNxkwwm0wqAZM'):
     flights = requests.get('https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey={0}&origin={1}&destination={2}&departure_date={3}'.format(api_key, origin, departure, date))
     json_data = json.loads(flights.text)
@@ -20,24 +53,26 @@ def pull_data(origin, departure, date, api_key='oIkAPQAiwHwJc7cUHCblNxkwwm0wqAZM
                     origin = d[i]['itineraries'][0]['outbound']['flights'][j]['origin']['airport']
                     destination = d[i]['itineraries'][0]['outbound']['flights'][j]['destination']['airport']
                     flight_path.append((origin, destination))
-            routes.append((flight_num, fare + tax, flight_path))
+            duration = d[i]['itineraries'][0]['outbound']['duration']
+            routes.append((flight_num, fare + tax, flight_path, duration))
     return sorted(routes, key=lambda x:x[1])
 
-def main():
-    origin = input('Input Initial Location. ')
-    departure = input('Input Destination. ')
-    date = input('Inpute Departure Date. ')
+def main(origin, departure, date):
+##    origin = input('Input Initial Location. ')
+##    departure = input('Input Destination. ')
+##    date = input('Inpute Departure Date. ')
     result = pull_data(origin, departure, date)
     if result == 'No matching results.':
         print(result)
     else:
         for x in result:
-            flight_number, cost, flight_path = x
+            flight_number, cost, flight_path, duration = x
+            duration = time_format(duration)
             if len(flight_path) == 1:
                 flight_path = 'Direct Flight'
             else:
                 flight_path = ' -> '.join([x[0] for x in flight_path] + [flight_path[-1][1]])
-            print('Flight {0:8} {2:40} ${1:.2f}'.format(flight_number, cost, flight_path))
+            print('Flight {0:8} {2:35} {3:30} ${1:.2f}'.format(flight_number, cost, flight_path, duration))
 
 if __name__ == '__main__':
-    main()
+    main('IST', 'BOS', '2018-12-01')
