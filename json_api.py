654f5cb3618f4a637008bbe4893c7ac2850ae53a
James Le <33073939+JamzJamzJamz@users.noreply.github.com>
2018-11-04 03:35:48 -0600

    Update json_api.py

--- json_api.py
+++ json_api.py
@@ -2,29 +2,28 @@ import requests
 import json
 
 def pull_data(origin, departure, date, api_key='oIkAPQAiwHwJc7cUHCblNxkwwm0wqAZM'):
-    flights = requests.get('https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey={0}&origin={1}&destination={2}&departure_date={3}'.format(api_key, origin, departure, date))
-    json_data = json.loads(flights.text)
-    #print(json_data)
-    if 'results' in json_data:
-            d = json_data['results']
-    else:
-            return 'No matching results.'
-    routes = []
-    for i, result in enumerate(d):
-            fare = float(d[i]['fare']['price_per_adult']['total_fare'])
-            tax = float(d[i]['fare']['price_per_adult']['tax'])
-            flight_num = d[i]['itineraries'][0]['outbound']['flights'][0]['flight_number']
-            routes.append((flight_num, fare + tax))
-    return sorted(routes, key=lambda x:x[1])
+        flights = requests.get('https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey={0}&origin={1}&destination={2}&departure_date={3}'.format(api_key, origin, departure, date))
+        json_data = json.loads(flights.text)
+	#print(json_data)
+        if 'results' in json_data:
+                d = json_data['results']
+        else:
+                return 'No matching results.'
+        routes = []
+        for i, result in enumerate(d):
+                fare = float(d[i]['fare']['price_per_adult']['total_fare'])
+                tax = float(d[i]['fare']['price_per_adult']['tax'])
+                flight_num = d[i]['itineraries'][0]['outbound']['flights'][0]['flight_number']
+                airline = d[i]['itineraries'][0]['outbound']['flights'][0]['marketing_airline']
+                num_of_flights = d[i]['itineraries'][0]['outbound']['flights']
+                flight_path = []
+                for j, value in enumerate(num_of_flights):
+                	origin = d[i]['itineraries'][0]['outbound']['flights'][j]['origin']['airport']
+                	destination = d[i]['itineraries'][0]['outbound']['flights'][j]['destination']['airport']
+                	flight_path.append((origin, destination))
+                #print(flight_path)
+                print("Flight {0}: ${1: .2f} Number of Flights: {2} Flight Path: {3}".format(flight_num, fare+tax, len(num_of_flights), flight_path))
+                routes.append((flight_num, fare + tax))
+        return sorted(routes, key=lambda x:x[1])
 
-#pull_data('HOU', 'AUS', '2018-11-10')
-
-def main():
-    origin = input('Input starting location. ')
-    departure = input('Input destination. ')
-    date = input('Inpute desired travel date. ')
-    for flight_num, total_cost in pull_data(origin, departure, date):
-            print('Flight {0}: ${1}'.format(flight_num, total_cost))
-
-if __name__ == '__main__':
-    main()
+pull_data('IAH', 'LDE', '2018-11-20')
